4332c8a59445aa0fdb6539f0eb1c70e99e60f0d4
NigelTanYong <nigel.ty28@gmail.com>
2024-02-23 18:16:47 +0800

    Basic list

--- ObjCGettingStarted/ObjCGettingStarted/ViewController.m
+++ ObjCGettingStarted/ObjCGettingStarted/ViewController.m
@@ -13,13 +13,29 @@
 
 @implementation ViewController
 
+NSString *cellId = @"cellId";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
     self.navigationItem.title = @"Courses";
     self.navigationController.navigationBar.prefersLargeTitles = YES;
+    
+    [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:cellId];
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+//    cell.backgroundColor = UIColor.redColor;
+    cell.textLabel.text = @"Course Name";
+    return cell;
+}
+
 
 
 @end
